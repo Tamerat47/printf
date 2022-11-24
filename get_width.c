9328dc151fbc5d34c22f7897d863dca6c10f5f62
root <root@LAPTOP-0MBVDSHJ>
2022-11-24 17:45:46 +0300

    ola

--- get_width.c
+++ get_width.c
@@ -1,30 +1,18 @@
 #include "main.h"
 
-
-
 /**
-
  * get_size - Calculates the size to cast the argument
-
  * @format: Formatted string in which to print the arguments
-
  * @i: List of arguments to be printed.
-
  *
-
  * Return: Precision.
-
  */
 
 int get_size(const char *format, int *i)
-
 {
 
 	int curr_i = *i + 1;
-
 	int size = 0;
-
-
 
 	if (format[curr_i] == 'l')
 
@@ -34,8 +22,6 @@ int get_size(const char *format, int *i)
 
 		size = S_SHORT;
 
-
-
 	if (size == 0)
 
 		*i = curr_i - 1;
@@ -44,8 +30,5 @@ int get_size(const char *format, int *i)
 
 		*i = curr_i;
 
-
-
 	return (size);
-
 }
